cf625b1b32364ec0f3d8558ab13eec7ce9c5418a
lcomrade <70049256+lcomrade@users.noreply.github.com>
2021-10-10 19:06:43 +0700

    Added more gradations for expiration

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -74,20 +74,66 @@ func randString() string {
 
 func expirParse(expiration string) (int64, error) {
 	switch expiration {
+	//Minutes
+	case "5m":
+		return 60 * 5, nil
+
 	case "10m":
 		return 60 * 10, nil
+
+	case "20m":
+		return 60 * 20, nil
 
 	case "30m":
 		return 60 * 30, nil
 
+	case "40m":
+		return 60 * 40, nil
+
+	case "50m":
+		return 60 * 50, nil
+
+	//Hours
 	case "1h":
 		return 60 * 60, nil
 
+	case "2h":
+		return 60 * 60 * 2, nil
+
+	case "4h":
+		return 60 * 60 * 4, nil
+
+	case "12h":
+		return 60 * 60 * 12, nil
+
+	//Days
 	case "1d":
 		return 60 * 60 * 24, nil
 
+	case "2d":
+		return 60 * 60 * 24 * 2, nil
+
+	case "3d":
+		return 60 * 60 * 24 * 3, nil
+
+	case "4d":
+		return 60 * 60 * 24 * 4, nil
+
+	case "5d":
+		return 60 * 60 * 24 * 5, nil
+
+	case "6d":
+		return 60 * 60 * 24 * 6, nil
+
+	//Weeks
 	case "1w":
 		return 60 * 60 * 24 * 7, nil
+
+	case "2w":
+		return 60 * 60 * 24 * 7 * 2, nil
+
+	case "3w":
+		return 60 * 60 * 24 * 7 * 3, nil
 	}
 
 	return 0, errors.New("unknown expiration: " + expiration)
