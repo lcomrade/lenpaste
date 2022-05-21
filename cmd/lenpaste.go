75a4b715d96fbe3c6800cf48435b82302e19137e
lcomrade <root@lcomrade.su>
2022-05-21 19:00:32 +0700

    Change license to AGPL v3

--- cmd/lenpaste.go
+++ cmd/lenpaste.go
@@ -1,23 +1,20 @@
-/*
-   Copyright 2021 Leonid Maslakov
+// Copyright (C) 2021-2022 Leonid Maslakov.
 
-   License: GPL-3.0-or-later
+// This file is part of Lenpaste.
 
-   This file is part of Lenpaste.
+// Lenpaste is free software: you can redistribute it
+// and/or modify it under the terms of the
+// GNU Affero Public License as published by the
+// Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
 
-   Lenpaste is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+// Lenpaste is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Affero Public License for more details.
 
-   Lenpaste is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with Lenpaste.  If not, see <https://www.gnu.org/licenses/>.
-*/
+// You should have received a copy of the GNU Affero Public License along with Lenpaste.
+// If not, see <https://www.gnu.org/licenses/>.
 
 package main
 
