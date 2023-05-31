6ad0609f9396dcb256a647df4714fc14137b78a6
Leonid Maslakov <root@lcomrade.su>
2023-05-31 13:12:09 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,159 +1,164 @@
 #!/bin/sh
 set -e
 
-RUN_CMD="lenpaste"
+fatal_comp() {
+	>&2 echo "$1"
+	>&2 echo ""
+	>&2 echo "Yes, I know that breaking backward compatibility is a bad idea. I'll try not to do it again:)"
+	>&2 echo ""
+	>&2 echo "More on what other backward compatibility was broken in Lenpaste v1.4 can be found here: https://lcomrade.su/rfQCH"
+	exit 1
+}
 
+fatal_comp_env() {
+	fatal_comp "$1 environment variable is no longer supported."
+}
+
+fatal_comp_l10n() {
+	fatal_comp "A single \"$1\" file is no longer supported. The \"$1\" file must be split into several files for different locales: \"$1/en.txt\", \"$1/ru.txt\" and etc."
+}
 
 # LENPASTE_ADDRESS
 if [ -n "$LENPASTE_ADDRESS" ]; then
-	RUN_CMD="$RUN_CMD -address $LENPASTE_ADDRESS"
+	fatal_comp_env "LENPASTE_ADDRESS"
 fi
 
 
 # LENPASTE_DB_DRIVER
 if [ -n "$LENPASTE_DB_DRIVER" ]; then
-	RUN_CMD="$RUN_CMD -db-driver '$LENPASTE_DB_DRIVER'"
+	fatal_comp_env "LENPASTE_DB_DRIVER"
 fi
 
 
 # LENPASTE_DB_SOURCE
-if [ -z "$LENPASTE_DB_DRIVER" ] || [ "$LENPASTE_DB_DRIVER" = "sqlite3" ]; then
-	RUN_CMD="$RUN_CMD -db-source /data/lenpaste.db"
-
-else
-	RUN_CMD="$RUN_CMD -db-source '$LENPASTE_DB_SOURCE'"
+if [ -n "$LENPASTE_DB_SOURCE" ]; then
+	fatal_comp_env "LENPASTE_DB_SOURCE"
 fi
 
 
 # LENPASTE_DB_MAX_OPEN_CONNS
 if [ -n "$LENPASTE_DB_MAX_OPEN_CONNS" ]; then
-	RUN_CMD="$RUN_CMD -db-max-open-conns '$LENPASTE_DB_MAX_OPEN_CONNS'"
+	fatal_comp_env "LENPASTE_DB_MAX_OPEN_CONNS"
 fi
 
 
 # LENPASTE_DB_MAX_IDLE_CONNS
 if [ -n "$LENPASTE_DB_MAX_IDLE_CONNS" ]; then
-	RUN_CMD="$RUN_CMD -db-max-idle-conns '$LENPASTE_DB_MAX_IDLE_CONNS'"
+	fatal_comp_env "LENPASTE_DB_MAX_IDLE_CONNS"
 fi
 
 
 # LENPASTE_DB_CLEANUP_PERIOD
 if [ -n "$LENPASTE_DB_CLEANUP_PERIOD" ]; then
-	RUN_CMD="$RUN_CMD -db-cleanup-period '$LENPASTE_DB_CLEANUP_PERIOD'"
+	fatal_comp_env "LENPASTE_DB_CLEANUP_PERIOD"
 fi
 
 # LENPASTE_ROBOTS_DISALLOW
-if [ "$LENPASTE_ROBOTS_DISALLOW" = "true" ]; then
-	RUN_CMD="$RUN_CMD -robots-disallow"
-	
-else
-	if [ "$LENPASTE_ROBOTS_DISALLOW" != "" ] && [ "$LENPASTE_ROBOTS_DISALLOW" != "false" ]; then
-		echo "[ENTRYPOINT] Error: unknown: LENPASTE_ROBOTS_DISALLOW = $LENPASTE_ROBOTS_DISALLOW"
-		exit 2
-	fi
+if [ -n "$LENPASTE_ROBOTS_DISALLOW" ]; then
+	fatal_comp_env "LENPASTE_ROBOTS_DISALLOW"
 fi
 
 
 # LENPASTE_TITLE_MAX_LENGTH
 if [ -n "$LENPASTE_TITLE_MAX_LENGTH" ]; then
-	RUN_CMD="$RUN_CMD -title-max-length '$LENPASTE_TITLE_MAX_LENGTH'"
+	fatal_comp_env "LENPASTE_TITLE_MAX_LENGTH"
 fi
 
 
 # LENPASTE_BODY_MAX_LENGTH
 if [ -n "$LENPASTE_BODY_MAX_LENGTH" ]; then
-	RUN_CMD="$RUN_CMD -body-max-length '$LENPASTE_BODY_MAX_LENGTH'"
+	fatal_comp_env "LENPASTE_BODY_MAX_LENGTH"
 fi
 
 
 # LENPASTE_MAX_PASTE_LIFETIME
 if [ -n "$LENPASTE_MAX_PASTE_LIFETIME" ]; then
-	RUN_CMD="$RUN_CMD -max-paste-lifetime '$LENPASTE_MAX_PASTE_LIFETIME'"
+	fatal_comp_env "LENPASTE_MAX_PASTE_LIFETIME"
 fi
 
 # Rate limits to get
 if [ -n "$LENPASTE_GET_PASTES_PER_5MIN" ]; then
-	RUN_CMD="$RUN_CMD -get-pastes-per-5min '$LENPASTE_GET_PASTES_PER_5MIN'"
+	fatal_comp_env "LENPASTE_GET_PASTES_PER_5MIN"
 fi
 
 if [ -n "$LENPASTE_GET_PASTES_PER_15MIN" ]; then
-	RUN_CMD="$RUN_CMD -get-pastes-per-15min '$LENPASTE_GET_PASTES_PER_15MIN'"
+	fatal_comp_env "LENPASTE_GET_PASTES_PER_15MIN"
 fi
 
 if [ -n "$LENPASTE_GET_PASTES_PER_1HOUR" ]; then
-	RUN_CMD="$RUN_CMD -get-pastes-per-1hour '$LENPASTE_GET_PASTES_PER_1HOUR'"
+	fatal_comp_env "LENPASTE_GET_PASTES_PER_1HOUR"
 fi
 
 
 # Rate limits to create
 if [ -n "$LENPASTE_NEW_PASTES_PER_5MIN" ]; then
-	RUN_CMD="$RUN_CMD -new-pastes-per-5min '$LENPASTE_NEW_PASTES_PER_5MIN'"
+	fatal_comp_env "LENPASTE_NEW_PASTES_PER_5MIN"
 fi
 
 if [ -n "$LENPASTE_NEW_PASTES_PER_15MIN" ]; then
-	RUN_CMD="$RUN_CMD -new-pastes-per-15min '$LENPASTE_NEW_PASTES_PER_15MIN'"
+	fatal_comp_env "LENPASTE_NEW_PASTES_PER_15MIN"
 fi
 
 if [ -n "$LENPASTE_NEW_PASTES_PER_1HOUR" ]; then
-	RUN_CMD="$RUN_CMD -new-pastes-per-1hour '$LENPASTE_NEW_PASTES_PER_1HOUR'"
+	fatal_comp_env "LENPASTE_NEW_PASTES_PER_1HOUR"
 fi
 
 
 
 # Server about
 if [ -f "/data/about" ]; then
-	RUN_CMD="$RUN_CMD -server-about /data/about"
+	fatal_comp_l10n "about"
 fi
 
 
 # Server rules
 if [ -f "/data/rules" ]; then
-	RUN_CMD="$RUN_CMD -server-rules /data/rules"
+	fatal_comp_l10n "rules"
 fi
 
 
 # Server terms of use
 if [ -f "/data/terms" ]; then
-	RUN_CMD="$RUN_CMD -server-terms /data/terms"
+	fatal_comp_l10n "terms"
 fi
 
 
 # LENPASTE_ADMIN_NAME
 if [ -n "$LENPASTE_ADMIN_NAME" ]; then
-	RUN_CMD="$RUN_CMD -admin-name '$LENPASTE_ADMIN_NAME'"
+	fatal_comp_env "LENPASTE_ADMIN_NAME"
 fi
 
 
 # LENPASTE_ADMIN_MAIL
 if [ -n "$LENPASTE_ADMIN_MAIL" ]; then
-	RUN_CMD="$RUN_CMD -admin-mail '$LENPASTE_ADMIN_MAIL'"
+	fatal_comp_env "LENPASTE_ADMIN_MAIL"
 fi
 
 
 # LENPASTE_UI_DEFAULT_LIFETIME
 if [ -n "$LENPASTE_UI_DEFAULT_LIFETIME" ]; then
-	RUN_CMD="$RUN_CMD -ui-default-lifetime '$LENPASTE_UI_DEFAULT_LIFETIME'"
+	fatal_comp_env "LENPASTE_UI_DEFAULT_LIFETIME"
 fi
 
 
 # LENPASTE_UI_DEFAULT_THEME
 if [ -n "$LENPASTE_UI_DEFAULT_THEME" ]; then
-	RUN_CMD="$RUN_CMD -ui-default-theme $LENPASTE_UI_DEFAULT_THEME"
+	fatal_comp_env "LENPASTE_UI_DEFAULT_THEME"
 fi
 
 
 # External UI themes
-if [ -d "/data/themes" ]; then
-	RUN_CMD="$RUN_CMD -ui-themes-dir /data/themes"
-fi
+# if [ -d "/data/themes" ]; then
+# 	# Themes must work normal.
+# 	# No alert needed.
+# fi
 
 
 # Lenpsswd file
 if [ -f "/data/lenpasswd" ]; then
-	RUN_CMD="$RUN_CMD -lenpasswd-file /data/lenpasswd"
+	fatal_comp "The \"lenpasswd\" file is no longer supported."
 fi
 
 
 # Run Lenpaste
-echo "[ENTRYPOINT] $RUN_CMD"
-sh -c "$RUN_CMD"
+lenpaste --cfg-dir /etc/lenpaste/ run
